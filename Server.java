68797b13e29fb055786066b0f76d75fe5b81c20c
Dragon740f <80297629+Dragon740f@users.noreply.github.com>
2023-05-20 20:47:15 +0800

    Server Version Aplha 1.1
    sorry for there hotfixs.  Now you can use /list to show user list.

--- Server.java
+++ Server.java
@@ -1,5 +1,5 @@
-import org.json.JSONObject;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.*;
 import java.net.ServerSocket;
@@ -16,15 +16,12 @@ public class Server {
     private String accessFile;
     private String srvmsg;
     private List<ClientHandler> clients;
-    private List<String> onlineUsers;
+    private static List<String> onlineUsers = new ArrayList<>();
 
     public Server() {
         loadConfig();
         clients = new ArrayList<>();
-        ServerVersion = "Alpha 1.1";
-        onlineUsers = new ArrayList<>();
-        private static List<String> onlineUsers = new ArrayList<>();
-
+        ServerVersion = "Alpha 1";
     }
 
     private void loadConfig() {
@@ -74,74 +71,27 @@ public class Server {
     }
 
     private String readFile(String filename) {
+        StringBuilder content = new StringBuilder();
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(filename));
-            StringBuilder content = new StringBuilder();
+            FileReader fileReader = new FileReader(filename);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
             String line;
-
-            while ((line = reader.readLine()) != null) {
+            while ((line = bufferedReader.readLine()) != null) {
                 content.append(line);
             }
-
-            reader.close();
-
-            return content.toString();
+            bufferedReader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        return null;
+        return content.toString();
     }
 
     private void writeFile(String filename, String content) {
         try {
-            FileWriter writer = new FileWriter(filename);
-            writer.write(content);
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void start() {
-        try {
-            // 创建ServerSocket对象，并绑定监听端口
-            ServerSocket serverSocket = new ServerSocket(port);
-
-            // 显示服务器启动消息
-            System.out.println("TinyMSG Server " + ServerVersion + " Started! Bind at " + port + " port, output file name is " + accessFile);
-
-            while (true) {
-                // 监听客户端的连接请求
-                Socket clientSocket = serverSocket.accept();
-
-                // 创建一个新的客户端处理器线程
-                ClientHandler clientHandler = new ClientHandler(clientSocket);
-                clients.add(clientHandler);
-
-                // 启动线程处理客户端连接
-                clientHandler.start();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void broadcastMessage(String message) {
-        // 广播消息给所有连接的客户端
-        for (ClientHandler client : clients) {
-            client.sendMessage(message);
-        }
-    }
-
-    private void writeToFile(String filename, String message) {
-        try {
-            FileWriter fileWriter = new FileWriter(filename, true);
+            FileWriter fileWriter = new FileWriter(filename);
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
             PrintWriter printWriter = new PrintWriter(bufferedWriter);
-
-            printWriter.println(message);
-
+            printWriter.println(content);
             printWriter.close();
             bufferedWriter.close();
             fileWriter.close();
@@ -150,105 +100,75 @@ public class Server {
         }
     }
 
-    public static void main(String[] args) {
-        Server server = new Server();
-        server.start();
-    }
-
     private class ClientHandler extends Thread {
         private Socket clientSocket;
-        private BufferedReader in;
         private PrintWriter out;
-        private String username;
-        private void sendOnlineUsers() {
-            StringBuilder userList = new StringBuilder();
-            userList.append("Online Users:\n");
-            for (String user : onlineUsers) {
-            userList.append(user).append("\n");
-            }
-            out.println(userList.toString());
-        }
+        private BufferedReader in;
 
-        public ClientHandler(Socket clientSocket) {
-            this.clientSocket = clientSocket;
-        }
-
-        public void run() {
+        public ClientHandler(Socket socket) {
+            clientSocket = socket;
             try {
-                // 获取输入流，用于接收客户端发送的数据
-                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
-                // 获取输出流，用于向客户端发送数据
                 out = new PrintWriter(clientSocket.getOutputStream(), true);
-
-                // 接收客户端发送的用户名
-                username = in.readLine();
-                System.out.println("USER " + username + " Connected");
-
-                // Add the username to the list of online users
-                synchronized (onlineUsers) {
-                    onlineUsers.add(username);
-                }
-
-                // 发送服务器消息给客户端
-                out.println(srvmsg);
-
-                while (true) {
-                    // 从客户端读取数据
-                    String clientMessage = in.readLine();
-
-                    if (clientMessage == null) {
-                        // 客户端已关闭连接
-                        break;
-                    }
-
-                    // 在服务器端打印接收到的数据
-                    System.out.println("Recived message from " + username + " : " + clientMessage);
-
-                    if (clientMessage.equalsIgnoreCase("/exit")) {
-                        // 客户端发送退出命令，断开连接
-                        break;
-                    }
-                    if (clientMessage.equalsIgnoreCase("/version")) {
-                        out.println("TinyMSG server version " + ServerVersion);
-                    }
-                    if (clientMessage.equalsIgnoreCase("/help")) {
-                        out.println("TinyMSG server version " + ServerVersion);
-                        out.println("/exit to Disconnect");
-                        out.println("/version to show Server Version");
-                        out.println("/list to show online users");
-                    }
-                    if (clientMessage.equalsIgnoreCase("/list")) {
-                        sendOnlineUsers();
-                    }
-
-                    // 将消息写入文件
-                    writeToFile(accessFile, clientMessage);
-
-                    // 广播消息给所有连接的客户端
-                    broadcastMessage(username + ": " + clientMessage);
-                }
-
-                // 关闭连接
-                clientSocket.close();
-
-                // Remove the username from the list of online users
-                synchronized (onlineUsers) {
-                    onlineUsers.remove(username);
-                }
-
-                // 从客户端处理器列表中移除当前客户端
-                clients.remove(this);
-
-                System.out.println("User " + username + " Disconnected");
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
 
-        public void sendMessage(String message) {
-            // 向客户端发送消息
-            out.println(message);
+        @Override
+        public void run() {
+            try {
+                String clientMessage;
+                while ((clientMessage = in.readLine()) != null) {
+                    // 处理客户端消息
+                    if (clientMessage.equalsIgnoreCase("/list")) {
+                        sendOnlineUsers();
+                    } else {
+                        // 其他处理逻辑
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    in.close();
+                    out.close();
+                    clientSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
+
+        private void sendOnlineUsers() {
+            StringBuilder userList = new StringBuilder();
+            userList.append("Online Users:\n");
+            synchronized (onlineUsers) {
+                for (String user : onlineUsers) {
+                    userList.append(user).append("\n");
+                }
+            }
+            out.println(userList.toString());
+        }
+    }
+
+    public void start() {
+        try {
+            ServerSocket serverSocket = new ServerSocket(port);
+            System.out.println("TinyMSG Server " + ServerVersion + " Started! Bind at " + port + " port, output file name is " + accessFile);
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                ClientHandler clientHandler = new ClientHandler(clientSocket);
+                clients.add(clientHandler);
+                clientHandler.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.start();
     }
 }
