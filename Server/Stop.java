468773bbffbc36beb521cbabab1567b0bf98510d
Glowingstone124 <80297629+glowingstone124@users.noreply.github.com>
2023-07-26 10:58:28 +0800

    Delete Stop.java

--- Server/Stop.java
+++
@@ -1,9 +0,0 @@
-package org.qo.tinymsg;
-import org.qo.tinymsg.Plugins.PluginLoader;
-public class Stop {
-    public static void stop(){
-        PluginLoader.stopPlugins();
-
-        System.exit(0);
-    }
-}
